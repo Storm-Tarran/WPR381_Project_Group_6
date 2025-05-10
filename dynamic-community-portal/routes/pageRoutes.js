3e3866b634ff0b1000186023d454fd0cd70b50f3
Storm Tarran <stormtarran03@gmail.com>
2025-05-10 16:57:16 +0200

    Prefilled data

---
+++ dynamic-community-portal/routes/pageRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+});
+
+router.get('/about', (req, res) => {
+});
+
+router.get('/events', (req, res) => {
+});
+
+router.get('/contact', (req, res) => {
+});
+
+router.get('/thankyou', (req, res) => {
+});
+
+module.exports = router;
